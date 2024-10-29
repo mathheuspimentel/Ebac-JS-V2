342a4e55cd3afbc893520eb9ecaf168d1ef54522
mathheuspimentel <121939802+mathheuspimentel@users.noreply.github.com>
2024-10-29 11:33:02 -0300

    Add files via upload

---
+++ V2/script.js
@@ -0,0 +1,9 @@
+function clikOk() {
+    
+    //exibe em console o valor armazenado
+    console.log('Parabéns, agora você está visualizando a mensagem');
+
+    document.getElementById("mensagem").innerText = "Parabéns, agora você está visualizando a mensagem";
+
+}
+
